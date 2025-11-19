226348e9dedf7d754a7509053d2d5409d0e13d04
chrissteck <mya.steckler@gmail.com>
2025-11-18 23:07:51 -0700

    Created header with navigation and weather and merged changes from 11/18.

--- index.js
+++ index.js
@@ -16,3 +16,24 @@ document.addEventListener('DOMContentLoaded', () => {
         apiData.innerHTML = <li>{$task}</li>
     }
 });
+
+
+
+function getWeather() {
+
+    const url = 'https://api.open-meteo.com/v1/forecast?latitude=45.676998&longitude=-111.042931&current_weather=true&temperature_unit=fahrenheit';
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const weather = data;
+
+        document.getElementById('temp').textContent = `Temperature: ${weather.current_weather.temperature}${weather.current_weather_units.temperature}`;
+        document.getElementById('wind').textContent = `Wind Speed: ${weather.current_weather.windspeed} ${weather.current_weather_units.windspeed}`;
+        document.getElementById('elevation').textContent = `Elevation: ${weather.elevation} feet`;
+
+    })
+    .catch(error => console.error("Error fetching weather:", error));
+}
+
+getWeather();
